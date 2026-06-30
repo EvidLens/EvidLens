aeef79b77fa1c68bad48f8a204f1225875eb7743
LensConnect254 <joram.mburun@gmail.com>
2026-06-30 21:13:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -90,3 +90,9 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+@app.route("/")
+def home():
+    return "LensConnect API is running"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
