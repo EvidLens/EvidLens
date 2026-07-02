169477bf37853f0ae4f178cd03c71c8917604562
LensConnect254 <joram.mburun@gmail.com>
2026-07-02 22:21:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./lensconnect.db")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 from sqlalchemy import func
 import random
 
