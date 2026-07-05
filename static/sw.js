1e88d6a831b167d5a87f97a11ea31d9dacdc3ce3
EvidLens <joram.mburun@gmail.com>
2026-07-05 05:25:57 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,2 +1,1 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('lensconnect-v1'))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+
