d3735449a5564c65429231c92afe816e2321aafc
EvidLens <joram.mburun@gmail.com>
2026-07-05 22:43:40 +0300

    Update models.py

--- app/modules/ai_agent/models.py
+++ app/modules/ai_agent/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
 from sqlalchemy.sql import func
-from app.core.db import Base
+from core.db import Base
 
 class AgentTask(Base):
     __tablename__ = "ai_agent_tasks"
