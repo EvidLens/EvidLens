0f5e965f4f7a9089e789f583598ac54914750c71
EvidLens <joram.mburun@gmail.com>
2026-07-06 01:37:43 +0300

    Update database.py

--- app/modules/database.py
+++ app/modules/database.py
@@ -5,7 +5,10 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"} if "postgresql" in DATABASE_URL else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
