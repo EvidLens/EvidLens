898f8282f7a9c3702dd454bfda8585db83c7742d
EvidLens <joram.mburun@gmail.com>
2026-07-07 21:14:00 +0300

    Create models.py

---
+++ app/modules/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.modules.db import Base
+
+class Sector(Base):
+    __tablename__ = "sectors"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+class County(Base):
+    __tablename__ = "counties" 
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    sector = Column(String)
