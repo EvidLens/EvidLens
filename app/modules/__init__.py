54afa344cb82f580c4dea5d68b73825fb4e2e6f4
EvidLens <joram.mburun@gmail.com>
2026-07-07 20:59:04 +0300

    Update __init__.py

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,17 +1,1 @@
-"""
-EvidLens Core Modules
-Kenya's Decision Intelligence Platform
-"""
 
-__version__ = "1.0.0"
-
-# Only import core services here. Don't import messaging to avoid circular import
-from . import database
-from . import auth
-from . import payments
-
-__all__ = [
-    "database",
-    "auth", 
-    "payments"
-]
