4de22c66117ac11a89fcb4983ec45c0f983543b3
EvidLens <joram.mburun@gmail.com>
2026-07-07 10:24:29 +0300

    Update __init__.py

--- app/modules/location_intel/__init__.py
+++ app/modules/location_intel/__init__.py
@@ -1,3 +1,3 @@
 from .router import router
-from .service import get_county_comparison, get_heatmap_data, fetch_osm_businesses
+from .service import get_county_comparison, fetch_osm_businesses
 from .models import LocationComparison, OpportunityHeatmap
