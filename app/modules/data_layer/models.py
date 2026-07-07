805913b0f4c6843bc7f5af8c2329d30cc8f66d02
EvidLens <joram.mburun@gmail.com>
2026-07-07 21:29:48 +0300

    Update models.py

--- app/modules/data_layer/models.py
+++ app/modules/data_layer/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, Enum, Index
 from sqlalchemy.sql import func
-from app.modules.database import Base
+from app.modules.db import Base
 import enum
 
 class DataSource(str, enum.Enum):
