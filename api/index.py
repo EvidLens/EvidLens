18de2bb02b09e9132d27bb50be75670e4bab5144
EvidLens <joram.mburun@gmail.com>
2026-07-09 20:07:41 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -98,3 +98,4 @@ app.include_router(business_router, prefix="/os", tags=["Lane 8: Business OS - E
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+handler = app
