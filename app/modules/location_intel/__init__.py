f3ee4e8950f7986ce0be1b5a22aa19c42638a841
EvidLens <joram.mburun@gmail.com>
2026-07-14 04:45:43 +0300

    Update __init__.py

--- app/modules/location_intel/__init__.py
+++ app/modules/location_intel/__init__.py
@@ -1,3 +1,29 @@
 from .router import router
-from .service import get_county_comparison, fetch_osm_businesses
-from .models import LocationComparison, OpportunityHeatmap
+from .service import (
+    get_location_comparison,
+    generate_heatmap,
+    calculate_price_arbitrage,
+    fetch_osm_businesses,
+    seed_geo_data,
+    get_coords
+)
+from .models import (
+    LocationComparison,
+    OpportunityHeatmap,
+    PriceArbitrage,
+    LocationGeo
+)
+
+__all__ = [
+    "router",
+    "get_location_comparison",
+    "generate_heatmap", 
+    "calculate_price_arbitrage",
+    "fetch_osm_businesses",
+    "seed_geo_data",
+    "get_coords",
+    "LocationComparison",
+    "OpportunityHeatmap",
+    "PriceArbitrage",
+    "LocationGeo"
+]
