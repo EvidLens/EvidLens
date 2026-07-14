cb6b9fc6da3b4355013318a2a9a476be2380f2b4
EvidLens <joram.mburun@gmail.com>
2026-07-14 04:58:02 +0300

    Update __init__.py

--- app/modules/market_engine/__init__.py
+++ app/modules/market_engine/__init__.py
@@ -1,3 +1,21 @@
 from .router import router
-from .service import search_market, get_competitor_overview, calculate_market_size
+from .service import (
+    search_market, 
+    get_competitor_overview, 
+    calculate_market_size,
+    get_price_stats,
+    infer_sector_from_query
+)
 from .models import MarketSearch, Competitor, MarketMetric
+
+__all__ = [
+    "router",
+    "search_market",
+    "get_competitor_overview", 
+    "calculate_market_size",
+    "get_price_stats",
+    "infer_sector_from_query",
+    "MarketSearch",
+    "Competitor", 
+    "MarketMetric"
+]
