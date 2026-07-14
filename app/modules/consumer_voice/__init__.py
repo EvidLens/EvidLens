e82e655dd16332e3ab05df55d84a45b49aa4d70c
EvidLens <joram.mburun@gmail.com>
2026-07-14 03:56:22 +0300

    Update __init__.py

--- app/modules/consumer_voice/__init__.py
+++ app/modules/consumer_voice/__init__.py
@@ -1,3 +1,5 @@
 from .router import router
 from .service import aggregate_sentiment, fetch_reddit_data
-from .models import ConsumerFeedback
+from .models import ConsumerFeedback, SentimentSummary, Sentiment, Source
+
+__all__ = ["router", "aggregate_sentiment", "fetch_reddit_data", "ConsumerFeedback", "SentimentSummary", "Sentiment", "Source"]
