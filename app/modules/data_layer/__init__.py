e91bf28c1f087bc164057d8f213edd70aa8c5796
EvidLens <joram.mburun@gmail.com>
2026-07-14 04:14:19 +0300

    Update __init__.py

--- app/modules/data_layer/__init__.py
+++ app/modules/data_layer/__init__.py
@@ -1,3 +1,17 @@
 from .router import router
-from .service import fetch_price_trends, fetch_demand_signals, fetch_location_analytics
-from .models import PriceTrend, DemandSignal, LocationMetric
+from .service import fetch_price_trends, fetch_demand_signals, fetch_location_analytics, seed_product_catalog
+from .models import PriceTrend, DemandSignal, LocationMetric, ProductCatalog, CompanyProfile, DataSource
+
+__all__ = [
+    "router",
+    "fetch_price_trends", 
+    "fetch_demand_signals", 
+    "fetch_location_analytics",
+    "seed_product_catalog",
+    "PriceTrend",
+    "DemandSignal", 
+    "LocationMetric",
+    "ProductCatalog",
+    "CompanyProfile",
+    "DataSource"
+]
