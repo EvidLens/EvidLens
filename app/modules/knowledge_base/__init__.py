228c613eb4d5ed15f9bea040add12eab87ed6686
EvidLens <joram.mburun@gmail.com>
2026-07-14 04:23:22 +0300

    Update __init__.py

--- app/modules/knowledge_base/__init__.py
+++ app/modules/knowledge_base/__init__.py
@@ -1,3 +1,14 @@
 from .router import router
-from .service import get_sector_report, search_knowledge, ingest_sector_data
-from .models import SectorReport, KnowledgeChunk
+from .service import get_sector_report, search_knowledge, ingest_sector_data, generate_report_with_groq
+from .models import SectorReport, KnowledgeChunk, KENYA_SECTORS
+
+__all__ = [
+    "router",
+    "get_sector_report",
+    "search_knowledge", 
+    "ingest_sector_data",
+    "generate_report_with_groq",
+    "SectorReport",
+    "KnowledgeChunk",
+    "KENYA_SECTORS"
+]
