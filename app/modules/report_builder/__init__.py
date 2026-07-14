416b34caf57f42c6d94a30017d14046cf8a2a777
EvidLens <joram.mburun@gmail.com>
2026-07-14 05:21:32 +0300

    Update __init__.py

--- app/modules/report_builder/__init__.py
+++ app/modules/report_builder/__init__.py
@@ -1,1 +1,20 @@
+from .router import router
+from .service import (
+    generate_market_report_pdf,
+    generate_market_report_excel,
+    generate_report_pdf # alias
+)
+from .models import Report, ReportType, ReportFormat, ReportStatus, ReportTemplate, ReportShare
 
+__all__ = [
+    "router",
+    "generate_market_report_pdf",
+    "generate_market_report_excel", 
+    "generate_report_pdf",
+    "Report",
+    "ReportType",
+    "ReportFormat", 
+    "ReportStatus",
+    "ReportTemplate",
+    "ReportShare"
+]
