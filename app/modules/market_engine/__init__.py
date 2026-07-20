b8bf797d731cea9a8507dac3ec8550eab3321c0b
EvidLens <joram.mburun@gmail.com>
2026-07-20 22:34:38 +0300

    Update __init__.py

--- app/modules/market_engine/__init__.py
+++ app/modules/market_engine/__init__.py
@@ -1,10 +1,14 @@
-from .router import router
-from .service import MarketEngineService
-from .models import MarketSearch, MarketMetric
+from.router import router
+from.service import MarketEngineService
+from.models import MarketSearch, MarketMetric, PriceTrend, DemandSignal, LocationMetric, ProductCatalog
 
 __all__ = [
     "router",
     "MarketEngineService",
     "MarketSearch",
-    "MarketMetric"
+    "MarketMetric",
+    "PriceTrend",
+    "DemandSignal",
+    "LocationMetric",
+    "ProductCatalog"
 ]
