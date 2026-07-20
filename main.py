af88e0101160a57f7db6318b65fd5d04d3577c55
EvidLens <joram.mburun@gmail.com>
2026-07-20 13:02:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.responses import HTMLResponse, StreamingResponse
 from dotenv import load_dotenv
 from sqlmodel import SQLModel, Field, Session, select, create_engine, or_
 from sqlalchemy import func, distinct, desc, asc
+from pydantic import BaseModel
 import os
 import requests
 import csv
@@ -22,7 +23,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=False) if DATABASE_URL else create_engine("sqlite:///./evidlens.db", connect_args={"check_same_thread": False})
 
-app = FastAPI(title="EvidLens API", version="2.5.2")
+app = FastAPI(title="EvidLens API", version="2.5.3")
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 PRICING = {"BASIC": {"monthly": 500, "yearly": 5000},"PROFESSIONAL": {"monthly": 1500, "yearly": 15000},"ENTERPRISE": {"monthly": 5000, "yearly": 50000}}
@@ -89,7 +90,7 @@ def seed_data(db: Session):
     if db.exec(select(Sector)).first(): return
     sectors = ["Agriculture","Livestock","Dairy","Poultry","Fisheries & Aquaculture","Forestry","Horticulture","Floriculture","Tea","Coffee","Sugar","Cotton","Mining","Oil & Gas","Quarrying","Manufacturing","Food Processing","Beverage Production","Textile & Apparel","Leather & Footwear","Furniture","Paper & Packaging","Printing & Publishing","Chemicals","Pharmaceuticals","Plastics & Rubber","Metal & Steel","Cement","Glass & Ceramics","Construction","Building Materials","Real Estate","Property Management","Architecture","Engineering Services","Energy","Renewable Energy","Electricity Supply","Water & Sanitation","Waste Management & Recycling","Wholesale Trade","Retail Trade","E-commerce","Automotive","Vehicle Assembly","Vehicle Sales","Vehicle Repair & Maintenance","Logistics","Transportation","Warehousing","Courier & Postal Services","Aviation","Maritime & Shipping","Rail Transport","Hospitality","Tourism","Travel Agencies","Restaurants & Catering","Financial Services","Banking","Insurance","SACCOs","Microfinance","FinTech","Information & Communication Technology (ICT)","Telecommunications","Software Development","Cybersecurity","Media & Broadcasting","Advertising & Marketing","Education & Training","Healthcare","Professional & Consulting Services","Public Administration & Government","NGOs, International Organizations & Development Partners"]
     for s in sectors: db.add(Sector(name=s))
-    counties_subs = {"Mombasa County": ["Changamwe", "Jomvu", "Kisauni", "Nyali", "Likoni", "Mvita"],"Kwale County": ["Msambweni", "Lunga Lunga", "Matuga", "Kinango"],"Kilifi County": ["Kilifi North", "Kilifi South", "Kaloleni", "Rabai", "Ganze", "Malindi", "Magarini"],"Tana River County": ["Garsen", "Galole", "Bura"],"Lamu County": ["Lamu East", "Lamu West"],"Taita-Taveta County": ["Taveta", "Wundanyi", "Mwatate", "Voi"],"Garissa County": ["Garissa Township", "Balambala", "Lagdera", "Dadaab", "Fafi", "Ijara"],"Wajir County": ["Wajir North", "Wajir East", "Tarbaj", "Wajir West", "Eldas", "Wajir South"],"Mandera County": ["Mandera West", "Banissa", "Mandera North", "Mandera South", "Mandera East", "Lafey"],"Marsabit County": ["Moyale", "North Horr", "Saku", "Laisamis"],"Isiolo County": ["Isiolo North", "Isiolo South"],"Meru County": ["Igembe South", "Igembe Central", "Igembe North", "Tigania West", "Tigania East", "North Imenti", "Buuri", "Central Imenti", "South Imenti"],"Tharaka-Nithi County": ["Maara", "Chuka/Igambang'ombe", "Tharaka"],"Embu County": ["Manyatta", "Runyenjes", "Mbeere South", "Mbeere North"],"Kitui County": ["Mwingi North", "Mwingi West", "Mwingi Central", "Kitui West", "Kitui Rural", "Kitui Central", "Kitui East", "Kitui South"],"Machakos County": ["Masinga", "Yatta", "Kangundo", "Matungulu", "Kathiani", "Mavoko", "Machakos Town", "Mwala"],"Makueni County": ["Mbooni", "Kilome", "Kaiti", "Makueni", "Kibwezi West", "Kibwezi East"],"Nyandarua County": ["Kinangop", "Kipipiri", "Ol Kalou", "Ol Jorok", "Ndaragwa"],"Nyeri County": ["Tetu", "Kieni East", "Kieni West", "Mathira East", "Mathira West", "Othaya", "Mukurweini", "Nyeri Central"],"Kirinyaga County": ["Mwea East", "Mwea West", "Gichugu", "Ndia", "Kirinyaga Central"],"Murang'a County": ["Kangema", "Mathioya", "Kiharu", "Kigumo", "Maragwa", "Kandara", "Gatanga"],"Kiambu County": ["Gatundu South", "Gatundu North", "Juja", "Thika Town", "Ruiru", "Githunguri", "Kiambu", "Kiambaa", "Kabete", "Kikuyu", "Limuru", "Lari"],"Turkana County": ["Turkana North", "Turkana West", "Turkana Central", "Loima", "Turkana South", "Turkana East"],"West Pokot County": ["Kapenguria", "Sigor", "Kacheliba", "Pokot South"],"Samburu County": ["Samburu West", "Samburu North", "Samburu East"],"Trans Nzoia County": ["Kwanza", "Endebess", "Saboti", "Kiminini", "Cherangany"],"Uasin Gishu County": ["Soy", "Turbo", "Moiben", "Ainabkoi", "Kapseret", "Kesses"],"Elgeyo-Marakwet County": ["Marakwet East", "Marakwet West", "Keiyo North", "Keiyo South"],"Nandi County": ["Tinderet", "Aldai", "Nandi Hills", "Chesumei", "Emgwen", "Mosop"],"Baringo County": ["Tiaty", "Baringo North", "Baringo Central", "Baringo South", "Mogotio", "Eldama Ravine"],"Laikipia County": ["Laikipia West", "Laikipia East", "Laikipia North"],"Nakuru County": ["Molo", "Njoro", "Naivasha", "Gilgil", "Kuresoi South", "Kuresoi North", "Subukia", "Rongai", "Bahati", "Nakuru Town West", "Nakuru Town East"],"Narok County": ["Kilgoris", "Emurua Dikirr", "Narok North", "Narok East", "Narok South", "Narok West"],"Kajiado County": ["Kajiado North", "Kajiado Central", "Kajiado East", "Kajiado West", "Kajiado South"],"Kericho County": ["Kipkelion East", "Kipkelion West", "Ainamoi", "Bureti", "Belgut", "Sigowet/Soin"],"Bomet County": ["Sotik", "Chepalungu", "Bomet East", "Bomet Central", "Konoin"],"Kakamega County": ["Lugari", "Likuyani", "Malava", "Lurambi", "Navakholo", "Mumias West", "Mumias East", "Matungu", "Butere", "Khwisero", "Shinyalu", "Ikolomani"],"Vihiga County": ["Vihiga", "Sabatia", "Hamisi", "Luanda", "Emuhaya"],"Bungoma County": ["Mt. Elgon", "Sirisia", "Kabuchai", "Bumula", "Kanduyi", "Webuye East", "Webuye West", "Kimilili", "Tongaren"],"Busia County": ["Teso North", "Teso South", "Nambale", "Matayos", "Butula", "Funyula", "Budalangi"],"Siaya County": ["Ugenya", "Ugunja", "Alego Usonga", "Gem", "Bondo", "Rarieda"],"Kisumu County": ["Kisumu East", "Kisumu West", "Kisumu Central", "Seme", "Nyando", "Muhoroni", "Nyakach"],"Homa Bay County": ["Kasipul", "Kabondo Kasipul", "Karachuonyo", "Rangwe", "Homa Bay Town", "Ndhiwa", "Mbita", "Suba"],"Migori County": ["Rongo", "Awendo", "Suna East", "Suna West", "Uriri", "Nyatike", "Kuria West", "Kuria East"],"Kisii County": ["Bonchari", "South Mugirango", "Bomachoge Borabu", "Bobasi", "Bomachoge Chache", "Nyaribari Masaba", "Nyaribari Chache", "Kitutu Chache North", "Kitutu Chache South"],"Nyamira County": ["Kitutu Masaba", "West Mugirango", "North Mugirango", "Borabu"],"Nairobi County": ["Westlands", "Dagoretti North", "Dagoretti South", "Lang'ata", "Kibra", "Roysambu", "Kasarani", "Ruaraka", "Embakasi South", "Embakasi North", "Embakasi Central", "Embakasi East", "Embakasi West", "Makadara", "Kamukunji", "Starehe", "Mathare"]}
+    counties_subs = {"Mombasa County": ["Changamwe", "Jomvu", "Kisauni", "Nyali", "Likoni", "Mvita"],"Kwale County": ["Msambweni", "Lunga Lunga", "Matuga", "Kinango"],"Kilifi County": ["Kilifi North", "Kilifi South", "Kaloleni", "Rabai", "Ganze", "Malindi", "Magarini"],"Tana River County": ["Garsen", "Galole", "Bura"],"Lamu County": ["Lamu East", "Lamu West"],"Taita-Taveta County": ["Taveta", "Wundanyi", "Mwatate", "Voi"],"Garissa County": ["Garissa Township", "Balambala", "Lagdera", "Dadaab", "Fafi", "Ijara"],"Wajir County": ["Wajir North", "Wajir East", "Tarbaj", "Wajir West", "Eldas", "Wajir South"],"Mandera County": ["Mandera West", "Banissa", "Mandera North", "Mandera South", "Mandera East", "Lafey"],"Marsabit County": ["Moyale", "North Horr", "Saku", "Laisamis"],"Isiolo County": ["Isiolo North", "Isiolo South"],"Meru County": ["Igembe South", "Igembe Central", "Igembe North", "Tigania West", "Tigania East", "North Imenti", "Buuri", "Central Imenti", "South Imenti"],"Tharaka-Nithi County": ["Maara", "Chuka/Igambang'ombe", "Tharaka"],"Embu County": ["Manyatta", "Runyenjes", "Mbeere South", "Mbeere North"],"Kitui County": ["Mwingi North", "Mwingi West", "Mwingi Central", "Kitui West", "Kitui Rural", "Kitui Central", "Kitui East", "Kitui South"],"Machakos County": ["Masinga", "Yatta", "Kangundo", "Matungulu", "Kathiani", "Mavoko", "Machakos Town", "Mwala"],"Makueni County": ["Mbooni", "Kilome", "Kaiti", "Makueni", "Kibwezi West", "Kibwezi East"],"Nyandarua County": ["Kinangop", "Kipiri", "Ol Kalou", "Ol Jorok", "Ndaragwa"],"Nyeri County": ["Tetu", "Kieni East", "Kieni West", "Mathira East", "Mathira West", "Othaya", "Mukurweini", "Nyeri Central"],"Kirinyaga County": ["Mwea East", "Mwea West", "Gichugu", "Ndia", "Kirinyaga Central"],"Murang'a County": ["Kangema", "Mathioya", "Kiharu", "Kigumo", "Maragwa", "Kandara", "Gatanga"],"Kiambu County": ["Gatundu South", "Gatundu North", "Juja", "Thika Town", "Ruiru", "Githunguri", "Kiambu", "Kiambaa", "Kabete", "Kikuyu", "Limuru", "Lari"],"Turkana County": ["Turkana North", "Turkana West", "Turkana Central", "Loima", "Turkana South", "Turkana East"],"West Pokot County": ["Kapenguria", "Sigor", "Kacheliba", "Pokot South"],"Samburu County": ["Samburu West", "Samburu North", "Samburu East"],"Trans Nzoia County": ["Kwanza", "Endebess", "Saboti", "Kiminini", "Cherangany"],"Uasin Gishu County": ["Soy", "Turbo", "Moiben", "Ainabkoi", "Kapseret", "Kesses"],"Elgeyo-Marakwet County": ["Marakwet East", "Marakwet West", "Keiyo North", "Keiyo South"],"Nandi County": ["Tinderet", "Aldai", "Nandi Hills", "Chesumei", "Emgwen", "Mosop"],"Baringo County": ["Tiaty", "Baringo North", "Baringo Central", "Baringo South", "Mogotio", "Eldama Ravine"],"Laikipia County": ["Laikipia West", "Laikipia East", "Laikipia North"],"Nakuru County": ["Molo", "Njoro", "Naivasha", "Gilgil", "Kuresoi South", "Kuresoi North", "Subukia", "Rongai", "Bahati", "Nakuru Town West", "Nakuru Town East"],"Narok County": ["Kilgoris", "Emurua Dikirr", "Narok North", "Narok East", "Narok South", "Narok West"],"Kajiado County": ["Kajiado North", "Kajiado Central", "Kajiado East", "Kajiado West", "Kajiado South"],"Kericho County": ["Kipkelion East", "Kipkelion West", "Ainamoi", "Bureti", "Belgut", "Sigowet/Soin"],"Bomet County": ["Sotik", "Chepalungu", "Bomet East", "Bomet Central", "Konoin"],"Kakamega County": ["Lugari", "Likuyani", "Malava", "Lurambi", "Navakholo", "Mumias West", "Mumias East", "Matungu", "Butere", "Khwisero", "Shinyalu", "Ikolomani"],"Vihiga County": ["Vihiga", "Sabatia", "Hamisi", "Luanda", "Emuhaya"],"Bungoma County": ["Mt. Elgon", "Sirisia", "Kabuchai", "Bumula", "Kanduyi", "Webuye East", "Webuye West", "Kimilili", "Tongaren"],"Busia County": ["Teso North", "Teso South", "Nambale", "Matayos", "Butula", "Funyula", "Budalangi"],"Siaya County": ["Ugenya", "Ugunja", "Alego Usonga", "Gem", "Bondo", "Rarieda"],"Kisumu County": ["Kisumu East", "Kisumu West", "Kisumu Central", "Seme", "Nyando", "Muhoroni", "Nyakach"],"Homa Bay County": ["Kasipul", "Kabondo Kasipul", "Karachuonyo", "Rangwe", "Homa Bay Town", "Ndhiwa", "Mbita", "Suba"],"Migori County": ["Rongo", "Awendo", "Suna East", "Suna West", "Uriri", "Nyatike", "Kuria West", "Kuria East"],"Kisii County": ["Bonchari", "South Mugirango", "Bomachoge Borabu", "Bobasi", "Bomachoge Chache", "Nyaribari Masaba", "Nyaribari Chache", "Kitutu Chache North", "Kitutu Chache South"],"Nyamira County": ["Kitutu Masaba", "West Mugirango", "North Mugirango", "Borabu"],"Nairobi County": ["Westlands", "Dagoretti North", "Dagoretti South", "Lang'ata", "Kibra", "Roysambu", "Kasarani", "Ruaraka", "Embakasi South", "Embakasi North", "Embakasi Central", "Embakasi East", "Embakasi West", "Makadara", "Kamukunji", "Starehe", "Mathare"]}
     for county, subs in counties_subs.items():
         c = County(name=county); db.add(c); db.commit(); db.refresh(c)
         for sub in subs: db.add(SubCounty(name=sub, county_id=c.id))
@@ -209,6 +210,51 @@ def get_opportunities(search: str = "", product: str = "", county: str = "", pag
     data = session.exec(q.offset((page-1)*limit).limit(limit)).all()
     return {"opportunities": [m.dict() for m in data], "total": total, "page": page}
 
+# <--- NEW DETAILED ANALYSIS ENDPOINT START --->
+class DetailedAnalysisRequest(BaseModel):
+    product: str
+    sector: str
+    county: str
+    subcounty: str = ""
+    budget_kes: float = 0
+    business_model: str = "Retail"
+
+@app.post("/api/analyze-detailed")
+async def analyze_detailed(req: DetailedAnalysisRequest, user_id: int = Depends(get_current_user), db: Session = Depends(get_db)):
+    check_subscription(user_id, db)
+
+    competitors = db.exec(select(Company).where(Company.sector==req.sector, Company.county==req.county).limit(10)).all()
+    prices = db.exec(select(MarketPrice).where(MarketPrice.product.contains(req.product), MarketPrice.county==req.county).limit(5)).all()
+    demand = db.exec(select(MarketMetric).where(MarketMetric.product_name.contains(req.product), MarketMetric.county==req.county).first())
+
+    prompt = f"""
+    You are EvidLens. Do a DETAILED market analysis for Kenya.
+    Product: {req.product}
+    Sector: {req.sector}
+    Location: {req.subcounty}, {req.county}
+    Budget: KES {req.budget_kes}
+    Model: {req.business_model}
+
+    Real Data Found:
+    Competitors: {[c.name for c in competitors]}
+    Avg Price: {[p.price for p in prices]}
+    Demand Score: {demand.demand_score if demand else 'N/A'}
+    Market Size: KES {demand.market_size_kes if demand else 'N/A'}
+
+    Return sections: 1. Executive Summary 2. Competitor Breakdown 3. Pricing Analysis 4. Demand & Opportunity 5. Risks 6. 3 Action Steps. Use KES, Counties. Be data-driven.
+    """
+
+    ai_response = generate_insights(prompt)
+    log_query(db, user_id)
+
+    return {
+        "summary": ai_response,
+        "competitors": [c.dict() for c in competitors],
+        "prices": [p.dict() for p in prices],
+        "demand": demand.dict() if demand else None
+    }
+# <--- NEW DETAILED ANALYSIS ENDPOINT END --->
+
 @app.get("/api/export/{table}")
 def export_csv(table: str, search: str = "", session: Session = Depends(get_session)):
     output = io.StringIO(); writer = csv.writer(output)
@@ -256,7 +302,7 @@ async def mpesa_callback(request: Request, db: Session = Depends(get_db)):
     return {"ResultCode": 0, "ResultDesc": "Accepted"}
 
 @app.get("/health")
-def health(): return {"status": "healthy", "version": "2.5.2"}
+def health(): return {"status": "healthy", "version": "2.5.3"}
 @app.get("/pricing", response_class=HTMLResponse)
 def pricing_page(request: Request): return templates.TemplateResponse("pricing.html", {"request": request, "plans": PRICING, "addons": ADDONS, "alc": ALC})
 @app.get("/privacy", response_class=HTMLResponse)
