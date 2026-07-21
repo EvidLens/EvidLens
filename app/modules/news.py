69551a82375a763194ababb16ae07edf3f8f1660
EvidLens <joram.mburun@gmail.com>
2026-07-21 03:16:54 +0300

    Delete app/modules/news.py

--- app/modules/news.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime
-
-class NewsArticle(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    product: str
-    title: str
-    timestamp: datetime = Field(default_factory=datetime.utcnow)
