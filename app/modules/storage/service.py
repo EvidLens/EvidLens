bea20756b792e2cf1c2f0fba53499caeee245653
EvidLens <joram.mburun@gmail.com>
2026-07-21 10:04:34 +0300

    Update service.py

--- app/modules/storage/service.py
+++ app/modules/storage/service.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_KEY = os.getenv("APP_SUPABASE_KEY")
 BUCKET = "evidlens-files"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
