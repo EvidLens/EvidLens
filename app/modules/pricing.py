7bfe13636ef80c2b32613593cfa04d7dbdeabcac
EvidLens <joram.mburun@gmail.com>
2026-07-21 03:17:08 +0300

    Delete app/modules/pricing.py

--- app/modules/pricing.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from datetime import datetime
-
-class PriceData(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    product_name: str
-    county: str
-    price: float
-    timestamp: datetime = Field(default_factory=datetime.utcnow)
