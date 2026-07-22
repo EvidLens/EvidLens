4a37314696d6d4e8e52c192a7c570885e941769a
EvidLens <joram.mburun@gmail.com>
2026-07-22 23:19:15 +0300

    Update __init__.py

--- app/modules/auth/__init__.py
+++ app/modules/auth/__init__.py
@@ -1,3 +1,5 @@
 from .router import router
 from .service import create_user, get_user_by_email, login_user
-from .models import AuthUser
+from .models import AuthUser, UserRole
+
+__all__ = ["router", "create_user", "get_user_by_email", "login_user", "AuthUser", "UserRole"]
