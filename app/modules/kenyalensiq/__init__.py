f5440c195939e8cad511053c6ce5d77861a95fd8
EvidLens <joram.mburun@gmail.com>
2026-07-22 21:40:24 +0300

    Update __init__.py

--- app/modules/kenyalensiq/__init__.py
+++ app/modules/kenyalensiq/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from App.kenyalensiq.router import router as lens_router
+from app.modules.kenyalensiq.router import router as lens_router
 
 router = APIRouter()
 router.include_router(lens_router, prefix="/kenyalensiq", tags=["KenyaLensIQ"])
