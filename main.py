9e4515c5cdd32ed65c97ec0c672e9e00a1394840
EvidLens <joram.mburun@gmail.com>
2026-07-23 02:33:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app.models.subscription import Subscription
+from app.models import Subscription
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
