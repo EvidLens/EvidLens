fcc7086abd0c2e96cf1f2dc352d90dad3d5702aa
EvidLens <joram.mburun@gmail.com>
2026-07-27 04:11:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from app.routers.api import router as api_router
+app.include_router(api_router)
 from app.routers.pages import router as pages_router
 app.include_router(pages_router)
 from app.modules.core.models import UserSubscription
