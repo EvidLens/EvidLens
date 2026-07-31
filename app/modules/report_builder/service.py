985ce0ef5b853e59e14c7420978fa684d28a76c8
EvidLens <joram.mburun@gmail.com>
2026-07-31 14:32:27 +0300

    Update service.py

--- app/modules/report_builder/service.py
+++ app/modules/report_builder/service.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 from fpdf import FPDF
 from sqlalchemy.orm import Session
-from app.modules.market_intel.service import search_market
+from app.modules.market_engine.service import search_market
 from app.modules.ai_insights.service import AIInsightsService
 from app.modules.knowledge_base.service import get_sector_benchmark
 
