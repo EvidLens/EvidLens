04f64b352a275e3581baab36fd2e60682f7f94bb
EvidLens <joram.mburun@gmail.com>
2026-07-31 12:22:10 +0300

    Update mpesa.py

--- app/modules/payments/mpesa.py
+++ app/modules/payments/mpesa.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 import requests
 import base64
 
-from app.modules.core.db import get_session, get_db
-from app.modules.core.models import User, Subscription
+from app.core.db import get_session, get_db
+from app.core.models import User, Subscription
 from app.modules.auth.dependencies import get_current_user
 from app.core.service import _core
 from app.core import settings
