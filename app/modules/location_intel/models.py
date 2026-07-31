80b161e6e9b4f2bd9b1366d34f5d224c065b542b
EvidLens <joram.mburun@gmail.com>
2026-07-31 15:12:45 +0300

    Update models.py

--- app/modules/location_intel/models.py
+++ app/modules/location_intel/models.py
@@ -14,6 +14,57 @@ KENYA_COUNTIES = [
     "Uasin Gishu", "Vihiga", "Wajir", "West Pokot"
 ]
 
+KENYA_SUBCOUNTIES = {
+    "Mombasa": ["Changamwe", "Jomvu", "Kisauni", "Nyali", "Likoni", "Mvita"],
+    "Kwale": ["Msambweni", "Lunga Lunga", "Matuga", "Kinango"],
+    "Kilifi": ["Kilifi North", "Kilifi South", "Kaloleni", "Rabai", "Ganze", "Malindi", "Magarini"],
+    "Tana River": ["Garsen", "Galole", "Bura"],
+    "Lamu": ["Lamu East", "Lamu West"],
+    "Taita-Taveta": ["Taveta", "Wundanyi", "Mwatate", "Voi"],
+    "Garissa": ["Garissa Township", "Balambala", "Lagdera", "Dadaab", "Fafi", "Ijara"],
+    "Wajir": ["Wajir North", "Wajir East", "Tarbaj", "Wajir West", "Eldas", "Wajir South"],
+    "Mandera": ["Mandera West", "Banisa", "Mandera North", "Mandera East", "Lafey", "Kutulo"],
+    "Marsabit": ["Moyale", "North Horr", "Saku", "Laisamis"],
+    "Isiolo": ["Isiolo North", "Isiolo South", "Garba Tulla"],
+    "Meru": ["Imenti North", "Imenti South", "Central Imenti", "Buuri", "Tigania East", "Tigania West", "Igembe North", "Igembe South", "Igembe Central"],
+    "Tharaka-Nithi": ["Nithi (Chuka/Igambang'ombe)", "Maara", "Tharaka"],
+    "Embu": ["Manyatta", "Runyenjes", "Mbeere South (Gachoka)", "Mbeere North (Siakago)"],
+    "Kitui": ["Kitui Central", "Kitui West", "Kitui Rural", "Kitui South", "Mutomo", "Mwingi North", "Mwingi Central", "Mwingi West"],
+    "Machakos": ["Machakos Town", "Mavoko", "Kathiani", "Matungulu", "Kangundo", "Mwala", "Yatta", "Masinga"],
+    "Makueni": ["Makueni", "Mbooni", "Kibwezi West", "Kibwezi East", "Kaiti", "Kilome"],
+    "Nyandarua": ["Kinangop", "Kipipiri", "Ol Kalou", "Ol Jorok", "Ndaragwa"],
+    "Nyeri": ["Nyeri Town", "Tetu", "Kieni", "Mathira", "Othaya", "Mukurweini"],
+    "Kirinyaga": ["Kirinyaga Central", "Kirinyaga East (Gichugu)", "Kirinyaga West (Ndia)", "Mwea East", "Mwea West"],
+    "Murang'a": ["Kiharu", "Kangema", "Mathioya", "Kigumo", "Maragwa", "Kandara", "Gatanga"],
+    "Kiambu": ["Thika Town", "Ruiru", "Githunguri", "Kiambu", "Kiambaa", "Kabete", "Kikuyu", "Limuru", "Lari", "Gatundu South", "Gatundu North", "Juja"],
+    "Turkana": ["Turkana Central", "Turkana North", "Turkana West", "Turkana South", "Turkana East", "Loima"],
+    "West Pokot": ["Kapenguria", "Sigor", "Kacheliba", "Pokot South"],
+    "Samburu": ["Samburu Central", "Samburu North", "Samburu East"],
+    "Trans Nzoia": ["Saboti", "Kiminini", "Cherangany", "Kwanza", "Endebess"],
+    "Uasin Gishu": ["Eldoret East", "Eldoret West", "Kesses", "Moiben", "Soy", "Turbo"],
+    "Elgeyo-Marakwet": ["Keiyo North", "Keiyo South", "Marakwet East", "Marakwet West"],
+    "Nandi": ["Nandi Hills", "Emgwen", "Chesumei", "Aldai", "Mosop", "Nandi Central"],
+    "Baringo": ["Baringo Central", "Baringo North", "Baringo South", "Mogotio", "Tiaty", "Eldama Ravine"],
+    "Laikipia": ["Laikipia East", "Laikipia West", "Laikipia North", "Nyahururu", "Ol Moran"],
+    "Nakuru": ["Nakuru Town East", "Nakuru Town West", "Naivasha", "Gilgil", "Molo", "Njoro", "Kuresoi North", "Kuresoi South", "Rongai", "Subukia"],
+    "Narok": ["Narok North", "Narok South", "Narok East", "Narok West", "Transmara West", "Transmara East"],
+    "Kajiado": ["Kajiado Central", "Kajiado North", "Kajiado East", "Kajiado West", "Kajiado South"],
+    "Kericho": ["Ainamoi", "Belgut", "Bureti", "Kipkelion East", "Kipkelion West", "Soin/Sigowet"],
+    "Bomet": ["Bomet Central", "Bomet East", "Chepalungu", "Konoin", "Sotik"],
+    "Kakamega": ["Lurambi", "Mumias East", "Mumias West", "Matungu", "Navakholo", "Khwisero", "Butere", "Shinyalu", "Ikolomani", "Lugari", "Likuyani"],
+    "Vihiga": ["Vihiga", "Sabatia", "Hamisi", "Emuhaya", "Luanda"],
+    "Bungoma": ["Kanduyi", "Bumula", "Kabuchai", "Kimilili", "Mt. Elgon", "Sirisia", "Tongaren", "Webuye East", "Webuye West"],
+    "Busia": ["Teso North", "Teso South", "Nambale", "Matayos", "Butula", "Funyula", "Budalangi"],
+    "Siaya": ["Alego Usonga", "Gem", "Ugenya", "Ugunja", "Bondo", "Rarieda"],
+    "Kisumu": ["Kisumu Central", "Kisumu East", "Kisumu West", "Seme", "Nyando", "Muhoroni", "Nyakach"],
+    "Homa Bay": ["Homa Bay Town", "Kasipul", "Kabondo Kasipul", "Karachuonyo", "Rangwe", "Ndhiwa", "Mbita", "Suba"],
+    "Migori": ["Migori East", "Migori West", "Rongo", "Awendo", "Uriri", "Nyatike", "Kuria East", "Kuria West"],
+    "Kisii": ["Kitutu Chache North", "Kitutu Chache South", "South Mugirango", "Bomachoge Borabu", "Bomachoge Chache", "Bobasi", "Nyaribari Chache", "Nyaribari Masaba", "Bonchari"],
+    "Nyamira": ["West Mugirango", "North Mugirango", "Kitutu Masaba", "Borabu"],
+    "Nairobi": ["Westlands", "Dagoretti North", "Dagoretti South", "Lang'ata", "Kibra", "Roysambu", "Kasarani", "Ruaraka", "Embakasi North", "Embakasi South", "Embakasi East", "Embakasi West", "Embakasi Central", "Makadara", "Kamukunji", "Starehe", "Mathare"]
+}
+
+
 class LocationGeo(SQLModel, table=True):
     __tablename__ = "location_geo"
 
