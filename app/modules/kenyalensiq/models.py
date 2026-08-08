fd0522d3d071e29ee80297f0da36526bda50075b
EvidLens <joram.mburun@gmail.com>
2026-08-08 16:19:55 +0300

    Update models.py

--- app/modules/kenyalensiq/models.py
+++ app/modules/kenyalensiq/models.py
@@ -1,16 +1,12 @@
-# app/modules/kenyalensiq/models.py
 from app.core.models import (
     KenyaLensSubscription,
     KenyaLensAlert, 
     KenyaLensMember,
     KenyaLensApiUsage,
-    KenyaLensBusiness,
-    KenyaLensSurvey,
     MarketMetric
 )
 
-# Re-export so old imports still work
 __all__ = [
     "KenyaLensSubscription", "KenyaLensAlert", "KenyaLensMember", 
-    "KenyaLensApiUsage", "KenyaLensBusiness", "KenyaLensSurvey", "MarketMetric"
+    "KenyaLensApiUsage", "MarketMetric"
 ]
